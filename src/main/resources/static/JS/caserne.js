d63f5934b27a2386b4b5a08c97c9fb5a5363ff30
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-05-24 17:39:03 +0200

    map + camions dynamiques

---
+++ src/main/resources/static/JS/caserne.js
@@ -0,0 +1,35 @@
+function aff_Camion(Camions) {
+
+    let template = document.getElementById('Camions');
+
+
+
+    for (const C of Camions) { // itère sur le tableau
+
+        let clone = document.importNode(template.content, true);      // clone le template
+        newContent2 = clone.firstElementChild.innerHTML // remplace {{modèle}}
+            .replace(/{{id}}/g, C.id) // et {{couleur}} par
+            .replace(/{{type}}/g, C.type)
+            .replace(/{{lon}}/g, C.lon)
+            .replace(/{{lat}}/g, C.lat)
+            .replace(/{{liquideType}}/g, C.liquideType)
+            .replace(/{{liquideQuantity}}/g, C.liquideQuantity)
+            .replace(/{{fuel}}/g, C.fuel)
+            .replace(/{{crewMember}}/g, C.crewMember)
+            .replace(/{{facilityRefId}}/g, C.facilityRefId)
+
+        clone.firstElementChild.innerHTML = newContent2
+        template.parentNode.appendChild(clone)}
+    console.log(template)
+}
+
+async function affichage_camions() {
+    const response = await fetch("JS/camion_test.json");
+    const camions_json = await response.json();
+    console.log(camions_json)
+    aff_Camion(camions_json);
+}
+
+$(document).ready(function () {
+    affichage_camions().then(r => console.log("camions affichés"));
+})
--- src/main/resources/static/JS/map.js
+++ src/main/resources/static/JS/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([45.75,  4.85], 13);
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
