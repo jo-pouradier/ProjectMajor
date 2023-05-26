465052cdd01ece5b4c9bdf8f2c93b096081dc89e
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-26 14:17:19 +0200

    facility service

--- src/main/java/fr/clbd/fire/service/FacilityService.java
+++ src/main/java/fr/clbd/fire/service/FacilityService.java
@@ -1,9 +1,34 @@
 package fr.clbd.fire.service;
 
+import fr.clbd.fire.model.dto.FacilityDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class FacilityService {
 
+    public List<FacilityDto> getAllFacilities() {
+        //TODO ???
+        return null;
+    }
+
+    public FacilityDto addFacility(FacilityDto facilityDto) {
+        //TODO ???
+        return null;
+    }
+
+    public boolean delAllFacilities() {
+        //TODO ???
+        return false;
+    }
+
+    public FacilityDto getFacility(UUID id) {
+        //TODO ???
+        return null;
+    }
+
+    // TODO get facility OBJECT
 
 }
