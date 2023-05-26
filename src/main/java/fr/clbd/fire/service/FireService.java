3ba7ffd5dac0bd9dca27c3d0f7d8628d91025aac
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-26 14:39:45 +0200

    fire service

--- src/main/java/fr/clbd/fire/service/FireService.java
+++ src/main/java/fr/clbd/fire/service/FireService.java
@@ -1,4 +1,24 @@
 package fr.clbd.fire.service;
 
+import fr.clbd.fire.model.dto.FireDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public class FireService {
+
+    public List<FireDto> getFires() {
+        // TODO
+        return null;
+    }
+
+    public FireDto getFire(UUID id) {
+        // TODO
+        return null;
+    }
+
+    public Integer getDistanceBetweenCoord(double lat1, double lon1, double lat2, double lon2) {
+        // TODO
+        return null;
+    }
 }
