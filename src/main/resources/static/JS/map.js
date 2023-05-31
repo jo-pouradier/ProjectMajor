f2ed95eaa5f78b6eebf908c30518c58f324cec65
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-31 16:14:27 +0200

    refactor, comments

--- src/main/resources/static/JS/map.js
+++ src/main/resources/static/JS/map.js
@@ -1,4 +1,5 @@
 const map = L.map('map').setView([45.75, 4.85], 13);
+// polygon representing the limit of the game
 const polygon = L.polygon([
     [45.65, 4.75],
     [45.65, 4.95],
@@ -20,6 +21,13 @@ const mapFireTypeColor = {
     'B_Alcohol': 'red'
 };
 
+let isLimitDisplay = false;
+
+/**
+ * @description Get the image from backend as a base64 string.
+ * @param url
+ * @returns {Promise<unknown>}
+ */
 function getImageString(url) {
     return fetch(url)
         .then((response) => {
@@ -46,10 +54,6 @@ function getImageString(url) {
         });
 }
 
-const fireIcon = createMarkerIcon("blue", "../images/blueFire.png");
-
-let isLimitDisplay = false;
-
 L.tileLayer(
     `https://tile.jawg.io/jawg-light/{z}/{x}/{y}.png?access-token=${token}`, {
         attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank" class="jawg-attrib">&copy; <b>Jawg</b>Maps</a> | <a href="https://www.openstreetmap.org/copyright" title="OpenStreetMap is open data licensed under ODbL" target="_blank" class="osm-attrib">&copy; OSM contributors</a>',
@@ -69,6 +73,13 @@ function createMarkerIcon(color, iconUrl) {
     });
 }
 
+/**
+ * @description Create a marker icon from a svg string.
+ * @param color the color of the icon
+ * @param svgString the svg file as string
+ * @returns {L.DivIcon}
+ * @todo make the type of the truck as a parameter, same goes for the class
+ */
 function createMarkerIconFromSvgString(color, svgString) {
     return L.divIcon({
         className: color + 'Truck',
@@ -102,11 +113,10 @@ async function displayTrucks() {
     uniqueFacilityRefID.forEach((facilityRefID, index) => {
         mapFacilityRefIDColor.set(facilityRefID, listColorTrucks[index]);
     });
-    console.log(trucksJson)
 
     trucksJson.forEach(truck => {
         color = mapFacilityRefIDColor.get(truck.facilityRefID) || "blue";
-        truckIcon.options.iconUrl = `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${color}.png`;
+        truckIcon.options.className = color + "Truck";
         const marker = L.marker([truck.lat, truck.lon], {icon: truckIcon,}).addTo(map);
         let desc = "";
         for (const [key, value] of Object.entries(truck)) {
@@ -116,15 +126,23 @@ async function displayTrucks() {
     });
 }
 
+/**
+ * @description Display all fires on the map.
+ * @returns {Promise<void>}
+ */
 async function displayFires() {
+    // get all fires from backend as dtos
     const firesResponse = await fetch("/fire/");
     const firesJson = await firesResponse.json();
+    // save fires in global variable
     listFires = firesJson;
     firesJson.forEach(fire => {
+        // set up a color for each fire type
         color = mapFireTypeColor.get(fire.fireType) || "blue";
-        fireIcon.options.iconUrl = `../images/${color}Fire.png`;
+        const fireIcon = createMarkerIcon("blue", `../images/${color}Fire.png`);
         const marker = L.marker([fire.lat, fire.lon], {icon: fireIcon}).addTo(map);
         let desc = "";
+        // set up the popup content with all fire properties
         for (const [key, value] of Object.entries(fire)) {
             desc += `<b>${key}: ${value}</b><br>`;
         }
@@ -133,6 +151,10 @@ async function displayFires() {
 }
 
 // TODO tester cette focntion avec les vraies données
+/**
+ * @description Display the bounding boxe on the map.
+ * @returns {Promise<void>}
+ */
 async function displayLimitSquare() {
     isLimitDisplay = !isLimitDisplay;
     if (isLimitDisplay) {
@@ -147,6 +169,9 @@ async function displayLimitSquare() {
 displayTrucks();
 displayFires();
 
+/**
+ * @description Display the menu on the map and replace it when closed.
+ */
 function toggleMenu() {
     let menu = document.getElementById('menu');
     if (menu.style.display === 'none') {
