113b887e4487d96d204c523750468f452016339f
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-06-02 15:32:18 +0200

    map ok

--- src/main/resources/static/JS/utils/displayMap.js
+++ src/main/resources/static/JS/utils/displayMap.js
@@ -1,22 +1,46 @@
-import {COLORS_FIRES, COLORS_TRUCKS, FIRE_TYPE} from "./EnumValues.js";
-import {getAllFacilities} from "./httpApi.js";
+import {COLORS_FIRES, FIRE_TYPE} from "./EnumValues.js";
+import {getAllFacilities, getAllFire} from "./httpApi.js";
 
-let listFiresIcon = [];
-let listFacilityIcon = [];
-let mapFacilityRefIDColor = new Map();
-let mapTruckJsonIcon = new Map();
-// setup color by facility refID
-getAllFacilities(setupColorByFacilityRefID, (err) => console.log(err));
-let color = "blue";
+/**
+    * @description Create a map with Jawg.io tiles.
+ */
 const map = L.map('map').setView([45.75, 4.85], 13);
 const jawgToken = "R6cQuklra3e9KwQdlLMguhgLQVrbAXRctK2fpXtSJpvI7VUWPdyZH0r0IrnRSlV9";
-
 
 L.tileLayer(
     `https://tile.jawg.io/jawg-light/{z}/{x}/{y}.png?access-token=${jawgToken}`, {
         attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank" class="jawg-attrib">&copy; <b>Jawg</b>Maps</a> | <a href="https://www.openstreetmap.org/copyright" title="OpenStreetMap is open data licensed under ODbL" target="_blank" class="osm-attrib">&copy; OSM contributors</a>',
     }
 ).addTo(map);
+
+
+/**
+ * @description create of team grouplayer and everybody group layer
+ * @description manage layer control
+ */
+
+let everybodyTruckGroupLayer = L.layerGroup().addTo(map);
+let teamTruckGroupLayer = L.layerGroup().addTo(map);
+let facilityTeamGroupLayer = L.layerGroup().addTo(map);
+let facilityEverybodyGroupLayer = L.layerGroup().addTo(map);
+let areaOfWork = L.layerGroup().addTo(map);
+let fireGroupLayer = L.layerGroup().addTo(map);
+var overlayMaps = {
+    "Area of work": areaOfWork,
+    "Everybody": everybodyTruckGroupLayer,
+    "Team": teamTruckGroupLayer,
+    "Facility Team": facilityTeamGroupLayer,
+    "Facility Everybody": facilityEverybodyGroupLayer,
+    "Fire": fireGroupLayer
+};
+var baseMaps = {
+}
+var layerControl = L.control.layers(baseMaps,overlayMaps).addTo(map);
+
+
+/**
+ *@description Create a polygon to show the game limit.
+ */
 
 const polygon = L.polygon([
     [45.6735, 4.9912],
@@ -25,33 +49,64 @@ const polygon = L.polygon([
     [45.826, 4.9912]
 
 ])
+polygon.addTo(map);
+polygon.addTo(areaOfWork);
 
-const displayLimitSquare = () => {
-    polygon.addTo(map);
-}
 
-const removeLimitSquare = () => {
-    map.removeLayer(polygon);
-}
+/**
+ * @description Create a mapFacilityRefIDColor to show the facility color.
+ */
 
-function setupColorByFacilityRefID(facilities) {
-    facilities.forEach((facility, index) => {
-        mapFacilityRefIDColor.set(facility.id, COLORS_TRUCKS[index]);
-    });
-    return mapFacilityRefIDColor;
-}
-
-const mapFireTypeColor = FIRE_TYPE.reduce((map, fireType, index) => {
+let mapFacilityRefIDColor = new Map();
+let mapTruckIcon = new Map();
+let mapFireTypeColor =new Map();
+FIRE_TYPE.reduce((map, fireType, index) => {
     map[fireType] = COLORS_FIRES[index];
     return map;
 }, {});
 
-function createMarkerIcon(color, iconUrl) {
-    return new L.Icon({
-        iconUrl: iconUrl,
+getAllFacilities(setupColorByFacilityRefID, (err) => console.log(err));
+
+/**
+    * @description Create a mapTruckRefIDColor to show the truck color. Blue for everybody and red for team.
+ *
+ */
+const teamFacilityId = [3796,3927];
+function setupColorByFacilityRefID(facilities) {
+    facilities.forEach((facility, index) => {
+        if (teamFacilityId.includes(facility.id)){
+            mapFacilityRefIDColor.set(facility.id, "red");
+        } else{
+            mapFacilityRefIDColor.set(facility.id, "blue");
+        }
+    });
+    return mapFacilityRefIDColor;}
+
+/**
+ * @description setup the color of the fire
+ */
+setupColorByFireType();
+function setupColorByFireType() {
+    FIRE_TYPE.forEach((type, index) => {
+        mapFireTypeColor.set(type, COLORS_FIRES[index]);
+    });
+    return mapFireTypeColor;
+}
+/**
+ * @description Create a marker icon from a svg string.
+ * @param color
+ * @param svgString
+ * @returns {*}
+ */
+
+
+function createMarkerIconFromSvgString(color, svgString) {
+    return L.divIcon({
+        className: color + 'Truck',
+        html: svgString,
         fill: color,
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
+        iconSize: [2, 2],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
@@ -59,7 +114,7 @@ function createMarkerIcon(color, iconUrl) {
 }
 
 /**
- * @description Get the image from backend as a base64 string.
+ * @description Create a marker icon from a svg string. with fetch to get the svg string
  * @param url
  * @returns {Promise<unknown>}
  */
@@ -90,123 +145,113 @@ function getImageString(url) {
 }
 
 /**
- * @description Create a marker icon from a svg string.
- * @param color the color of the icon
- * @param svgString the svg file as string
- * @returns L.divIcon the icon to display on the map
- * @todo make the type of the truck as a parameter, same goes for the class
+ * @description display facility on the map
+ * @type {*[]}
+ * @param listFacilityIcon : list of facility icon
  */
-function createMarkerIconFromSvgString(color, svgString) {
-    return L.divIcon({
-        className: color + 'Truck',
-        html: svgString,
-        fill: color,
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [35, 35],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
-}
 
-/**
- * @description Display all trucks on the map.
- * @param trucksJson list of trucks as json
- * @returns {Promise<void>}
- */
-async function displayTrucks(trucksJson) {
-    if (mapTruckJsonIcon.size !== 0) {
-        // look for trucks that have moved
-        // TODO this filter part doesn't work (trucks are blinking without moving)
-        let currentTrucks = Array.from(mapTruckJsonIcon.entries());
-        trucksJson.filter((truck, index, array) => {
-            currentTrucks.forEach(currentTruckJsonIcon => {
-                if (truck.facilityRefID === currentTruckJsonIcon[0].facilityRefID && (truck.lon !== currentTruckJsonIcon[0].lon || truck.lat !== currentTruckJsonIcon[0].lat)) {
-                    // the truck has moved, we remove it from the map
-                    mapTruckJsonIcon.delete(currentTruckJsonIcon[0]);
-                    map.removeLayer(currentTruckJsonIcon[1]);
-                    // return the truck for further processing
-                    return truck;
-                }
-            })
-        })
-    }
-
-    // fetch truck icon as svg string to create a marker icon
-    const truckSvgStringBase64 = await getImageString('/image/svg/truck.svg');
-    const truckSvg = atob(truckSvgStringBase64.split(',')[1]);
-    const truckIcon = createMarkerIconFromSvgString("blue", truckSvg);
-
-    // display each remaining trucks on the map
-    trucksJson.forEach(truck => {
-        color = mapFacilityRefIDColor.get(truck.facilityRefID) || "blue";
-        truckIcon.options.className = color + "Truck";
-        const marker = L.marker([truck.lat, truck.lon], {icon: truckIcon,}).addTo(map);
-        mapTruckJsonIcon.set(truck, marker);
-        let desc = "";
-        for (const [key, value] of Object.entries(truck)) {
-            desc += `<b>${key}: ${value}</b><br>`;
-        }
-        marker.bindPopup(desc);
-    });
-}
+let listFacilityIcon = [];
 
 async function displayFacility(facilityJson) {
-    listFacilityIcon.forEach(facility => {
-        map.removeLayer(facility);
-    });
-    listFacilityIcon = [];
 
     const facilitySvgStringBase64 = await getImageString('/image/svg/facility.svg');
     const facilitySvg = atob(facilitySvgStringBase64.split(',')[1]);
-    const facilityIcon = createMarkerIconFromSvgString("blue", facilitySvg);
 
     facilityJson.forEach(facility => {
-        const color = mapFacilityRefIDColor.get(facility.id) || "blue";
-        facilityIcon.options.className = color + "Facility facility";
-        const marker = L.marker([facility.lat, facility.lon], {icon: facilityIcon}).addTo(map);
-        listFacilityIcon.push(marker);
+        let facilityIcon = createMarkerIconFromSvgString(mapFacilityRefIDColor.get(facility.id), facilitySvg); //create icon and add color
+        facilityIcon.options.className = mapFacilityRefIDColor.get(facility.id) + "Facility facility"; //add class to icon
+
+        const marker = L.marker([facility.lat, facility.lon], {icon: facilityIcon}).addTo(map); //create marker and add icon to marker
         let desc = "";
         for (const [key, value] of Object.entries(facility)) {
             desc += `<b>${key}: ${value}</b><br>`;
+        } //create description for popup with all the facility information
+        marker.bindPopup(desc); //add popup to marker
+        listFacilityIcon.push(marker);
+
+        if (teamFacilityId.includes(facility.id)) {
+            marker.addTo(facilityTeamGroupLayer); //add marker to facilityTeamGroupLayer
+        } else {
+            marker.addTo(facilityEverybodyGroupLayer); //add marker to facilityEverybodyGroupLayer
         }
-        marker.bindPopup(desc);
     });
 }
 
 /**
- * @description Display all fires on the map.
+ * @description display for first time truck on the map
+ * @type {*[]}
+ */
+async function displayTrucks(truckJson) {
+    const truckSvgStringBase64 = await getImageString('/image/svg/truck.svg');
+    const truckSvg = atob(truckSvgStringBase64.split(',')[1]);
+
+    truckJson.forEach(truck => {
+        let truckIcon = createMarkerIconFromSvgString(mapFacilityRefIDColor.get(truck.facilityRefID), truckSvg); //create icon and add color
+        truckIcon.options.className = mapFacilityRefIDColor.get(truck.facilityRefID) + "Facility facility"; //add class to icon
+
+        const marker = L.marker([truck.lat, truck.lon], {icon: truckIcon}).addTo(map); //create marker and add icon to marker
+        let desc = "";
+        for (const [key, value] of Object.entries(truck)) {
+            desc += `<b>${key}: ${value}</b><br>`;
+        } //create description for popup with all the truck information
+        marker.bindPopup(desc); //add popup to marker
+        mapTruckIcon.set(truck.id, marker);
+        if (teamFacilityId.includes(truck.facilityRefID)) {
+            marker.addTo(teamTruckGroupLayer); //add marker to teamTruckGroupLayer
+        } else {
+            marker.addTo(everybodyTruckGroupLayer); //add marker to everybodyTruckGroupLayer
+        }
+    });
+}
+
+/**
+ * @description update truck position on the map
+ * @param trucksjson
  * @returns {Promise<void>}
  */
-async function displayFires(firesJson) {
-    listFiresIcon.forEach(fire => {
-        map.removeLayer(fire);
-    });
-    listFiresIcon = [];
 
-    firesJson.forEach(fire => {
-        // set up a color for each fire type
-        color = mapFireTypeColor.get(fire.fireType) || "blue";
-        const fireIcon = createMarkerIcon("blue", `../images/${color}Fire.png`);
-        const marker = L.marker([fire.lat, fire.lon], {icon: fireIcon}).addTo(map);
-        listFiresIcon.push(marker);
+async function updateTruckPos(trucksjson) {
+        trucksjson.forEach(truckjson => {
+            let truck = mapTruckIcon.get(truckjson.id);
+            truck.setLatLng([truckjson.lat, truckjson.lon]);
+        });
+}
+
+/**
+ * @description display fire position from the server
+ */
+
+function createMarkerIcon(iconUrl) {
+    return new L.Icon({
+        iconUrl: iconUrl,
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [20, 20],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [20, 20]
+    });
+}
+
+
+async function displayFirePos(fireJson) {
+    fireJson.forEach(fire => {
+        console.log(fire);
+        let color = mapFireTypeColor.get(fire.type) || "blue";
+        const fireIcon = createMarkerIcon( `../images/${color}Fire.png`);
+        fireIcon.options.className = "fire"; //add class to icon
+
+        const marker = L.marker([fire.lat, fire.lon], {icon: fireIcon}).addTo(map); //create marker and add icon to marker
         let desc = "";
-        // set up the popup content with all fire properties
         for (const [key, value] of Object.entries(fire)) {
             desc += `<b>${key}: ${value}</b><br>`;
-        }
-        marker.bindPopup(desc);
+        } //create description for popup with all the fire information
+        marker.bindPopup(desc); //add popup to marker
+        marker.addTo(fireGroupLayer); //add marker to fireGroupLayer
     });
 }
-
 export {
-    createMarkerIcon,
-    createMarkerIconFromSvgString,
+    displayFacility,
     displayTrucks,
-    displayFires,
-    getImageString,
-    displayLimitSquare,
-    removeLimitSquare,
-    displayFacility
+    updateTruckPos,
+    displayFirePos
 }
-
